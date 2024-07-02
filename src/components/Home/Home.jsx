e3f5537f46c27d54994714b278cc26ae34791525
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-07-02 23:04:12 +0600

    refactor: Update Home and Tshirt components
    - Add Cart component to Home component
    - Update Home component layout with CSS classes
    - Add Tshirt CSS class to Tshirt component
    
    Closes #123

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../Tshirt/Tshirt';
-
+import Cart from '../../Cart/Cart';
+import './Home.css';
 const Home = () => {
     const thirsts = useLoaderData();
     return (
-        <div>
-            {
+        <div className='home-container'>
+           <div className='t-shirts-container'>
+           {
                 thirsts.map(thirst => <Tshirt
                     key={thirst._id}
                     thirst={thirst}
                 ></Tshirt>)
             }
+           </div>
+           <div className="cart-container">
+              <Cart></Cart>
+           </div>
         </div>
     );
 };
 
 export default Home;
+ 
--- src/components/Tshirt/Tshirt.jsx
+++ src/components/Tshirt/Tshirt.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Tshirt.css';
 
 const Tshirt = (thirst) => {
      const {name, price, picture} = thirst.thirst;
     return (
-        <div>
+        <div className='t-shirt'>
             <img src={picture} alt="" />
         </div>
     );
