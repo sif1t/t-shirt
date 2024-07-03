050479cc2f99c127b80b0af39bd0a0988bd2b086
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-07-03 22:28:42 +0600

    refactor: Update Home component to include Cart and handleAddToCart function

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Tshirt from '../Tshirt/Tshirt';
 import Cart from '../../Cart/Cart';
 import './Home.css';
 const Home = () => {
     const thirsts = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = thirsts => {
+        console.log(thirsts);
+    }
     return (
         <div className='home-container'>
-           <div className='t-shirts-container'>
-           {
-                thirsts.map(thirst => <Tshirt
-                    key={thirst._id}
-                    thirst={thirst}
-                ></Tshirt>)
-            }
-           </div>
-           <div className="cart-container">
-              <Cart></Cart>
-           </div>
+            <div className='t-shirts-container'>
+                {
+                    thirsts.map(thirst => <Tshirt
+                        key={thirst._id}
+                        thirst={thirst}
+                        handleAddToCart={handleAddToCart}
+                    ></Tshirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
 
 export default Home;
- 
